e88be49ea301c6429d8070a3e3e3e11ff4dd64b3
wjlee611 <wjlee611@gmail.com>
2024-05-11 11:56:47 +0900

    Feat: "Implement FCM for android"
    Android의 prod, dev 빌드 환경에서의 FCM을 구현했습니다.
    fore/background, terminate 상태에서 모두 수신 및 앱 내 이벤트 트리거를 확인했습니다.

---
+++ lib/blocs/fcm/fcm_cubit.dart
@@ -0,0 +1,59 @@
+import 'package:bloc/bloc.dart';
+import 'package:child_goods_store_flutter/constants/strings.dart';
+import 'package:child_goods_store_flutter/models/res/res_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FCMCubit extends Cubit<FCMState> {
+  final String fcmToken;
+
+  FCMCubit({
+    required this.fcmToken,
+  }) : super(const FCMState()) {
+    // FCM listening
+    // at foreground
+    FirebaseMessaging.onMessage.listen(listenFCM);
+    // at background
+    FirebaseMessaging.onMessageOpenedApp.listen(listenFCM);
+    // at terminate
+    FirebaseMessaging.instance.getInitialMessage().then(listenFCM);
+  }
+
+  void listenFCM(RemoteMessage? message) {
+    if (message != null && fcmToken != Strings.nullStr) {
+      emit(FCMState.fromRemoteMessage(message));
+    }
+    emit(const FCMState());
+  }
+}
+
+class FCMState extends Equatable {
+  /// 푸쉬 알림에서 보여지는 문자열
+  final String? body;
+
+  /// FCM의 data 객체 데이터를 저장하는 객체
+  /// code: 어떤 타입의 데이터인지 정의
+  /// message: 문자열로 인코딩된 json 데이터. 파싱 방법은 code에 따라 상이.
+  final ResModel? data;
+
+  const FCMState({
+    this.body,
+    this.data,
+  });
+
+  factory FCMState.fromRemoteMessage(RemoteMessage? message) {
+    if (message != null && message.notification != null) {
+      return FCMState(
+        body: message.notification?.body,
+        data: ResModel.fromJson(message.data, (json) => null),
+      );
+    }
+    return const FCMState();
+  }
+
+  @override
+  List<Object?> get props => [
+        body,
+        data,
+      ];
+}
